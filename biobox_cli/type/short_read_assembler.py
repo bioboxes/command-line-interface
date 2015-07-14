cb09971079d4b36f5fa6c0d107e86f28270dec45
Michael Barton <mail@michaelbarton.me.uk>
2015-07-14 14:44:19 +0100

    Execute container
    Signed-off-by: Michael Barton <mail@michaelbarton.me.uk>

--- biobox_cli/type/short_read_assembler.py
+++ biobox_cli/type/short_read_assembler.py
@@ -13,6 +13,7 @@ Options:
 import biobox_cli.container   as ctn
 import biobox_cli.util        as util
 import biobox_cli.biobox_file as fle
+
 import os
 import tempfile as tmp
 
@@ -21,16 +22,32 @@ def run(argv):
 
     image      = opts['<image>']
     fastq_file = opts['--input']
-    biobox_args = fle.fastq_arguments(cntr_src_dir, [fastq_file, "paired"])
-
     if not ctn.image_available(image):
         msg = "No Docker image available with the name: {}"
         util.err_exit(msg.format(image))
 
-    host_src_dir = os.path.dirname(fastq_file)
-    host_dst_dir = tmp.mkdtemp()
-    host_yml_dir = fle.create_biobox_directory(fle.generate(biobox_args))
 
     cntr_src_dir = "/fastq"
-    cntr_dst_dir = "/assembly"
+    cntr_dst_dir = "/bbx/output"
     cntr_yml_dir = "/bbx/input"
+
+    biobox_args = fle.fastq_arguments(cntr_src_dir, [fastq_file, "paired"])
+
+    # Need to ensure is always an absolute path
+    host_src_dir = os.path.abspath(os.path.dirname(fastq_file))
+    host_dst_dir = tmp.mkdtemp()
+    host_yml_dir = fle.create_biobox_directory(fle.generate([biobox_args]))
+
+
+    import docker.utils
+
+    container = ctn.client().create_container(image, 'default',
+      volumes     = [cntr_src_dir, cntr_dst_dir, cntr_yml_dir],
+      host_config = docker.utils.create_host_config(binds=[
+          ":".join([host_src_dir, cntr_src_dir, "ro"]),
+          ":".join([host_yml_dir, cntr_yml_dir, "ro"]),
+          ":".join([host_dst_dir, cntr_dst_dir, "rw"])
+          ])
+      )
+
+    ctn.client().start(container.get("Id"))
--- console.py
+++ console.py
@@ -1,2 +1,4 @@
 import rlcompleter, readline
 readline.parse_and_bind('tab:complete')
+
+import biobox_cli.container as ctn
