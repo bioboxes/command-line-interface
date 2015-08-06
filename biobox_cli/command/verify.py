28b193dd8eaba5d78719175c9e53327bb2dc71b8
Michael Barton <mail@michaelbarton.me.uk>
2015-08-06 16:58:31 -0700

    Implement verification interface
    Signed-off-by: Michael Barton <mail@michaelbarton.me.uk>

--- biobox_cli/command/verify.py
+++ biobox_cli/command/verify.py
@@ -13,7 +13,28 @@ Available Biobox types:
 """
 
 import biobox_cli.util as util
+import sys, os, os.path, tempfile
+
+def verification_file(biobox):
+    file_ = biobox + '.feature'
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'verification', file_))
+
+def tmp_feature_dir():
+    return os.path.abspath(os.path.join(os.getcwd(), 'biobox_verify'))
 
 def run(argv):
-    opts = util.parse_docopt(__doc__, argv, False)
-    util.select_module("biobox_type", opts["<biobox_type>"]).run(argv)
+    opts   = util.parse_docopt(__doc__, argv, False)
+    biobox = opts['<biobox_type>']
+    image  = opts['<image>']
+
+    from behave.__main__ import main as behave_main
+    _, tmp_file = tempfile.mkstemp()
+    cmd = "{} --define TMP_DIR={} --define IMAGE={} --outfile {} --no-summary --stop"
+    behave_main(cmd.format(verification_file(biobox), tmp_feature_dir(), image, tmp_file))
+
+    with open(tmp_file, 'r') as f:
+        output = f.read()
+
+    if "Assertion Failed" in output:
+        msg = "Verification failed - {} is not a valid biobox {}."
+        util.err_exit(msg.format(image, biobox.replace('_', ' ')))
