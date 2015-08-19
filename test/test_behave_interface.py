77f34a73fbbcf9b66e5e7c349f05de8e4ad4d8d8
Michael Barton <mail@michaelbarton.me.uk>
2015-08-18 20:50:51 -0700

    Refactor behave_interface tests
    Signed-off-by: Michael Barton <mail@michaelbarton.me.uk>

--- test/test_behave_interface.py
+++ test/test_behave_interface.py
@@ -1,29 +1,30 @@
 import nose.tools                  as nt
 import biobox_cli.behave_interface as behave
 
+def feature(statuses):
+    feature_state = "failed" if ("failed" in statuses) else "passing"
+    return {'status'   : feature_state,
+            'elements' : map(scenario, statuses)}
+
+def scenario(status = "failed"):
+    return {"keyword" : "Scenario",
+            "name"    : "scenario name",
+            "steps"   : [{'result' : {'status' : status}}]} 
+
+
 def test_get_failing_for_single_pass():
-    passing = {"keyword" : "Scenario",
-                "name"    : "scenario name",
-                "steps"   : [{'result' : {'status' : 'passed'}}]} 
-    result = [{'status'   : 'passed',
-               'elements' : [passing]}]
-    nt.assert_equal([], behave.get_failing(result))
+    features = [feature(["passing"])]
+    nt.assert_equal([], behave.get_failing(features))
 
 def test_get_failing_for_single_failure():
-    scenario = {"keyword" : "Scenario",
-                "name"    : "scenario name",
-                "steps"   : [{'result' : {'status' : 'failed'}}]} 
-    result = [{'status'   : 'failed',
-               'elements' : [scenario]}]
-    nt.assert_equal([scenario], behave.get_failing(result))
+    features = [feature(["failed"])]
+    nt.assert_equal([scenario()], behave.get_failing(features))
 
 def test_get_failing_for_pass_and_failure():
-    passing = {"keyword" : "Scenario",
-                "name"    : "scenario name",
-                "steps"   : [{'result' : {'status' : 'passed'}}]} 
-    failing = {"keyword" : "Scenario",
-                "name"    : "scenario name",
-                "steps"   : [{'result' : {'status' : 'failed'}}]} 
-    result = [{'status'   : 'failed',
-               'elements' : [failing, passing]}]
-    nt.assert_equal([failing], behave.get_failing(result))
+    features = [feature(["failed", "passing"])]
+    nt.assert_equal([scenario()], behave.get_failing(features))
+
+def test_get_failing_for_multiple_failing_scenarios():
+    features = [feature(["failed", "passing"]),
+                feature(["failed", "passing"])]
+    nt.assert_equal([scenario(), scenario()], behave.get_failing(features))
