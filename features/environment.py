af2c75a4a99f93e3cae1a1d2d0485a88cce833b7
Michael Barton <mail@michaelbarton.me.uk>
2016-07-18 16:08:18 -0700

    Update location of feature tests are run in

--- features/environment.py
+++ features/environment.py
@@ -6,10 +6,9 @@ def before_scenario(context, _):
     root_dir = pt.abspath(pt.join(pt.dirname(__file__), '..'))
 
     path        = ":" + pt.join(root_dir, 'bin')
-    tmp         = pt.join(root_dir, "tmp")
+    tmp         = pt.join(root_dir, "tmp", "feature")
     python_path = pt.join(root_dir, 'vendor', 'python', 'lib', 'python2.7', 'site-packages')
 
     os.environ['PATH']       = path + ":" + os.environ['PATH']
     os.environ['PYTHONPATH'] = python_path
-    os.environ['TMPDIR']     = tmp  # Required to work with boot2docker
     context.env = TestFileEnvironment(base_path = tmp)
