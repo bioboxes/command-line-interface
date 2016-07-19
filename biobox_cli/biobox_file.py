434c83fc38121aed4847fc4c0d5500ee118f007b
Michael Barton <mail@michaelbarton.me.uk>
2016-07-19 15:09:29 -0700

    Use biobox.py to run the short_read_assembler biobox

--- biobox_cli/biobox_file.py
+++ biobox_cli/biobox_file.py
@@ -5,7 +5,7 @@ def generate(args):
     output = {"version" : "0.9.0", "arguments" : args}
     return yaml.safe_dump(output, default_flow_style = False)
 
-def parse(dir_):
+def get_biobox_file_contents(dir_):
     with open(os.path.join(dir_, 'biobox.yaml'), 'r') as f:
         return yaml.load(f.read())
 
