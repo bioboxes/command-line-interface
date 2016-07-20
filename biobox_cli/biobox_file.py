f76c8ca15112c1b86049a3bf6238d99d0198e95f
Michael Barton <mail@michaelbarton.me.uk>
2016-07-20 13:36:34 -0700

    Remove no longer needed functions for creating biobox file

--- biobox_cli/biobox_file.py
+++ biobox_cli/biobox_file.py
@@ -8,19 +8,3 @@ def generate(args):
 def get_biobox_file_contents(dir_):
     with open(os.path.join(dir_, 'biobox.yaml'), 'r') as f:
         return yaml.load(f.read())
-
-def fastq_arguments(args):
-    return files_values("fastq", args)
-
-def fasta_arguments(args):
-    return files_values("fasta", args)
-
-def reference_argument(ref):
-    return {"fasta_dir": [{"id" : 1, "type" : "reference", "value" : ref}]}
-
-def files_values(identifier, args):
-    values = [entry(identifier + "_" + str(i), p_c, t) for (i, (p_c, t)) in enumerate(args)]
-    return {identifier : values}
-
-def entry(id_, value, type_):
-    return {"id" : id_, "value" : value, "type" : type_}
