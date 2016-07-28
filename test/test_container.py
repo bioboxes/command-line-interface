73fe4a71c9edf52e93c794f91173b0aefeb9aaa1
Michael Barton <mail@michaelbarton.me.uk>
2016-07-28 11:33:43 -0700

    Add non-ci test for container removal

--- test/test_container.py
+++ test/test_container.py
@@ -1,3 +1,17 @@
-import helper               as hlpr
-import biobox_cli.container as ctn
-import biobox.util          as docker
+import pytest, tempfile, helper
+import biobox_cli.command.run as biobox
+
+
+def create_args(output):
+    return ["run",
+            "short_read_assembler",
+            "bioboxes/velvet",
+            "--input={}".format(helper.verification_file('short_read_assembler/genome_paired_reads.fq.gz')),
+            "--output={}".format(output)]
+
+@pytest.mark.no_ci
+@pytest.mark.slow
+def test_short_read_assembler():
+    # Will run the remove() function
+    # Difficult to check this has worked though
+    biobox.run(create_args(tempfile.mkdtemp()))
